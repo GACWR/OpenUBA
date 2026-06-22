79aecbc75c5133b9a9ae2803ee0d89e84752b341
jovonni <jovonnipharr@gmail.com>
2026-06-21 20:02:07 -0400

    fix(license): use full legal name "Georgia Cyber Warfare Range" in copyright text

--- core/display_test.py
+++ core/display_test.py
@@ -1,5 +1,5 @@
 '''
-Copyright (c) 2019–present GACWR
+Copyright (c) 2019–present Georgia Cyber Warfare Range
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
